0a13c707c7eb4623bc7b9400450ebcced3c49dc8
Sulthan Alam <sulam3010@gmail.com>
2020-09-21 22:28:00 +0700

    Updates

---
+++ precache-manifest.b118ad782a15e10763b7e57d044a6b5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cf4b5e5cfd566c63829",
+    "url": "/nim-finder/static/js/app.c5db8314.chunk.js"
+  },
+  {
+    "revision": "cf3f1a2d711f002e6237",
+    "url": "/nim-finder/static/js/runtime~app.bfc33435.js"
+  },
+  {
+    "revision": "751da372fa1d53325d0e",
+    "url": "/nim-finder/static/js/2.7006a5cf.chunk.js"
+  },
+  {
+    "revision": "f3f68bf5b7dce8acae55c25f724625dd",
+    "url": "/nim-finder/expo-service-worker.js"
+  },
+  {
+    "revision": "26c59aa851761b5f348838338f27a2cf",
+    "url": "/nim-finder/index.html"
+  },
+  {
+    "revision": "caae71402b169fe12edbecad04d53997",
+    "url": "/nim-finder/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/nim-finder/serve.json"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/nim-finder/static/js/2.7006a5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/nim-finder/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/nim-finder/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/nim-finder/favicon.ico"
+  },
+  {
+    "revision": "3e4933a25bf931e1f9e7386224099cb5",
+    "url": "/nim-finder/manifest.json"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,37 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nim-finder/precache-manifest.b118ad782a15e10763b7e57d044a6b5d.js"
+);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nim-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
+
+workbox.routing.registerRoute(/^https?.*/, workbox.strategies.networkFirst({ "cacheName":"offlineCache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"purgeOnQuotaError":false})] }), 'GET');
